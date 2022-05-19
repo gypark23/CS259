2f30e9c0497dc67b54f5e2b4b1d1207fbc4d7b76
Kyu Park <gypark23@uchicago.edu>
2022-05-19 16:13:05 -0500

    legend added

--- Assignment 8/task1.py
+++ Assignment 8/task1.py
@@ -89,11 +89,13 @@ for i in range (7, 63):
     xvar_pred.append(i)
     yvar_pred.append(0.0037 * e ** (1.882 * i))    #exponential equation derived from excel
 
-plt.plot(xvar, yvar)
+fig, ax = plt.subplots()
+ax.plot(xvar, yvar, 'blue', label='measured')
 plt.title("Consumption of Energy vs Number of Leading Zeroes (With Predictions)", fontsize = 8)
 plt.xlabel("Number of Leading Zeroes")
 plt.ylabel("Consumption of Energy (Joules)")
-plt.plot(xvar_pred, yvar_pred, 'r--')
+ax.plot(xvar_pred, yvar_pred, 'r--', label = 'Expected')
+ax.legend(loc='best', frameon=False)
 plt.savefig("figure.png", dpi = 500)
 plt.clf()
 plt.plot(xvar, yvar)
